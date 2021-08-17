8c83fa5a2ea11b5dbfacc153aa1bd72a225c5a11
Mikael O. Hansen <mih@delegate.dk>
2021-08-17 23:44:44 +0200

    Synapse added

---
+++ azure-cli/signalr/deploy.ps1
@@ -0,0 +1,70 @@
+<#
+  .SYNOPSIS
+  Deploys SignalR instance
+
+  .DESCRIPTION
+  The deploy.ps1 script deploys an Azure SignalR instance using the CLI tool to a resource group in the relevant environment.
+
+  .PARAMETER environmentType
+  Specifies the environment type. Staging (DevTest) or production
+
+  .PARAMETER location
+  Specifies the location where the services are deployed. Default is West Europe
+
+  .PARAMETER resourceGroupName
+  Specifies the name of the resource group
+
+  .PARAMETER signalRName
+  Specifies the name of the SignalR
+
+  .PARAMETER resourceTags
+  Specifies the tag elements that will be used to tag the deployed services
+
+  .INPUTS
+  None. You cannot pipe objects to deploy.ps1.
+
+  .OUTPUTS
+  None. deploy.ps1 does not generate any output.
+#>
+param (
+  [Parameter(Mandatory = $false)]
+  [ValidateNotNullOrEmpty()]
+  [ValidateSet('DevTest', 'Production')]
+  [string]
+  $environmentType = "DevTest",
+
+  [Parameter(Mandatory = $false)]
+  [ValidateNotNullOrEmpty()]
+  [string]
+  $location = "westeurope",
+
+  [Parameter(Mandatory = $true)]
+  [ValidateNotNullOrEmpty()]
+  [string]
+  $resourceGroupName,
+
+  [Parameter(Mandatory = $true)]
+  [ValidateNotNullOrEmpty()]
+  [string]
+  $signalRName,
+
+  [Parameter(Mandatory = $false)]
+  [string[]] $resourceTags = @()
+)
+
+#############################################################################################
+# Provision SignalR
+#############################################################################################
+Write-Host "Provision SignalR" -ForegroundColor DarkGreen
+
+Write-Host "  Creating SignalR" -ForegroundColor DarkYellow
+$output = az signalr create `
+  --name $signalRName `
+  --location $location `
+  --resource-group $resourceGroupName `
+  --sku Standard_S1 `
+  --unit-count 1 `
+  --allowed-origins * `
+  --service-mode Default
+
+Throw-WhenError -output $output
