264360d32a458c01bf997f8178e5428d44a511c7
lupusbytes <bertram@lupusbytes.com>
2022-03-10 19:37:02 +0100

    style(appservice): fix indentation

--- azure-cli/appservice/get_AppServicePlanId.ps1
+++ azure-cli/appservice/get_AppServicePlanId.ps1
@@ -1,21 +1,21 @@
 function Get-AppServicePlanId {
-    param (
-      [Parameter(Mandatory=$true)]
-      [string]
-      $appServicePlanName,
+  param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $appServicePlanName,
 
-      [Parameter(Mandatory=$true)]
-      [string]
-      $resourceGroup
-    )
+    [Parameter(Mandatory = $true)]
+    [string]
+    $resourceGroup
+  )
 
-    Write-Host "  Querying app service plan id" -ForegroundColor DarkYellow
-    $appServicePlanId = az appservice plan show `
-      --name $appServicePlanName `
-      --resource-group $resourceGroup `
-      --query id
+  Write-Host "  Querying app service plan id" -ForegroundColor DarkYellow
+  $appServicePlanId = az appservice plan show `
+    --name $appServicePlanName `
+    --resource-group $resourceGroup `
+    --query id
 
-    Throw-WhenError -output $appServicePlanId
+  Throw-WhenError -output $appServicePlanId
 
-    return $appServicePlanId
+  return $appServicePlanId
 }
