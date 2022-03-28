3f177e898519ed52a220aee37bc4fc063ad96bf2
Noblix <oliver@stenberg-jakobsen.dk>
2022-03-28 15:57:24 +0200

    fix: Use DarkYellow for logging that is indented

--- azure-cli/ad/deploy.swagger.ps1
+++ azure-cli/ad/deploy.swagger.ps1
@@ -46,19 +46,19 @@ $clientId = az ad app create `
 $objectId = az ad sp show --id $clientId --query objectId
 
 if (!$objectId) {
-  Write-Host "  Creating Service Principal for Swagger App Registration" -ForegroundColor DarkGreen
+  Write-Host "  Creating Service Principal for Swagger App Registration" -ForegroundColor DarkYellow
 
   $objectId = az ad sp create --id $clientId --query objectId
 }
 
-Write-Host "  Granting Swagger SPN access to App" -ForegroundColor DarkGreen
+Write-Host "  Granting Swagger SPN access to App" -ForegroundColor DarkYellow
 az ad app permission grant --id $clientId --api $appId
 Start-Sleep -Seconds 30 # This ARBITRARY delay time is required otherwise next call to grant admin consent will fail (SOMETIMES!)
 
-Write-Host "  Giving Admin permission" -ForegroundColor DarkGreen
+Write-Host "  Giving Admin permission" -ForegroundColor DarkYellow
 az ad app permission admin-consent --id $clientId
 
-Write-Host "  Setting redirect URIs" -ForegroundColor DarkGreen
+Write-Host "  Setting redirect URIs" -ForegroundColor DarkYellow
 $swaggerAppObjectId = az ad app show --id $clientId --query objectId
 
 $redirectUri = "https://$($NamingConfig.ServiceAbbreviation).$($EnvironmentConfig.EnvironmentName).$CompanyHostName/swagger/oauth2-redirect.html"
@@ -75,7 +75,7 @@ az rest `
   --headers "Content-Type=application/json" `
   --body "{\""spa\"":{\""redirectUris\"":$redirectUris}}"
 
-Write-Host "  Granting group access to api SPN" -ForegroundColor DarkGreen
+Write-Host "  Granting group access to api SPN" -ForegroundColor DarkYellow
 if ($EnvironmentConfig.environmentType -ne "Production") {
   Add-GroupToServicePrincipal -EnvironmentConfig $EnvironmentConfig -NamingConfig $NamingConfig -GroupId "GROUP-ID"
 }
