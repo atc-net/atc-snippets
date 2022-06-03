a46b1f2ce72b8a4060838b88b8482d953b6addb1
Lau Johansson <ljo@delegate.dk>
2022-06-03 14:45:03 +0200

    change indentation and rename ResourceName

--- azure-cli/utilities/Lock-Resource.ps1
+++ azure-cli/utilities/Lock-Resource.ps1
@@ -1,37 +1,36 @@
-
 function Lock-Resource {
-    # Lock creation needs the the service principal to have access: Microsoft.Authorization/locks/write
-    # Find resourcetypes: https://docs.microsoft.com/en-us/azure/templates/
-    param (
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [string]
-        $ResourceGroupName,
+  # Lock creation needs the service principal to have access: Microsoft.Authorization/locks/write
+  # Find resourcetypes: https://docs.microsoft.com/en-us/azure/templates/
+  param (
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $ResourceGroupName,
 
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [string]
-        $Resource,
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $ResourceName,
 
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [string]
-        $ResourceType,
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $ResourceType,
 
-        [Parameter(Mandatory = $true)]
-        [ValidateSet("CanNotDelete", "ReadOnly")]
-        [string]
-        $LockType
-    )
-  
-    Write-Host "  Creating '$LockType' lock for resource '$Resource' in '$ResourceGroupName'" -ForegroundColor DarkYellow
-    $output = az lock create `
-        --name $LockType `
-        --resource-group $ResourceGroupName `
-        --resource $Resource `
-        --lock-type $LockType `
-        --resource-type $ResourceType
+    [Parameter(Mandatory = $true)]
+    [ValidateSet("CanNotDelete", "ReadOnly")]
+    [string]
+    $LockType
+  )
 
-    Throw-WhenError -output $output
+  Write-Host "  Creating '$LockType' lock for resource '$ResourceName' in '$ResourceGroupName'" -ForegroundColor DarkYellow
+  $output = az lock create `
+    --name $LockType `
+    --resource-group $ResourceGroupName `
+    --resource $ResourceName `
+    --lock-type $LockType `
+    --resource-type $ResourceType
+
+  Throw-WhenError -output $output
 }
 
