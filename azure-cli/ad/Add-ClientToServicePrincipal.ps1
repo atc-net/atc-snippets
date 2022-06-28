8c8dcca5bdddae8c2a01f8b769ec9e407e9b8116
Thomas Malowanczyk <tkm@delegate.dk>
2022-06-28 14:49:07 +0200

    Add script to assing client id pre authorized access on app registration
    Generally used to assign Visual Studio access to allow for debugging locally

---
+++ azure-cli/ad/Add-ClientToServicePrincipal.ps1
@@ -0,0 +1,34 @@
+function Add-ClientToServicePrincipal {
+  param (
+    [Parameter(Mandatory = $true)]
+    [EnvironmentConfig] $environmentConfig,
+
+    [Parameter(Mandatory = $true)]
+    [NamingConfig] $namingConfig,
+
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $applicationId
+  )
+
+  # import utility functions
+  . "$PSScriptRoot\..\utilities\deploy.naming.ps1"
+
+  $appIdentityId = Get-AppIdentityUri `
+      -type "api" `
+      -environmentConfig $environmentConfig `
+      -namingConfig $namingConfig
+
+  $appId = az ad app list `
+      --identifier-uri $appIdentityId `
+      --query [-1].id
+
+  $graphApiUri = "https://graph.microsoft.com/v1.0/applications/" + $appId
+  $properties = az rest --method get --uri $graphApiUri | ConvertFrom-Json
+
+  $scopeAppId = $properties.api.oauth2PermissionScopes.id
+  $body = '{""api"": {""preAuthorizedApplications"": [{""appId"": ""' + $applicationId + '"",""delegatedPermissionIds"": [""' + $scopeAppId + '""]}]}}'
+
+  az rest --method patch --uri $graphApiUri --headers "Content-Type=application/json" --body $body
+}
